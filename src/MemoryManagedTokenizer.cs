e4c827eb072c6f30f7abd4252b9a622d925457d8
Andrew Hall <ryzngard@live.com>
2018-08-23 00:30:26 -0700

    Start reaading more from the stream

--- src/MemoryManagedTokenizer.cs
+++ src/MemoryManagedTokenizer.cs
@@ -42,93 +42,99 @@ namespace Chipotle.CSV
             return Task.FromResult<ISection>(null);
         }
 
-        protected override Task Read(CancellationToken token)
+        protected override async Task Read(CancellationToken token)
         {
             _memoryPoolRentSize = Math.Min(_memoryPool.MaxBufferSize, (int)Stream.Length);
 
             var memoryOwner = _memoryPool.Rent(_memoryPoolRentSize);
-            int bytesRead = 0;
-            bool consumeSectionDelimiters = false;
             var segmentHints = new List<int>();
-
-            int memoryStartIndex = 0;
             int rentedChunks = 1;
+            int consumedMemoryIndex = 0;
 
             while (!Disposed)
             {
-                // Read to the end of the line in the stream
-                int b = Stream.ReadByte();
+                int memoryStartIndex = 0;
+                int bytesRead = await Stream.ReadAsync(memoryOwner.Memory.Slice(consumedMemoryIndex));
+                bool consumeSectionDelimiters = false;
 
-                if (b == -1)
+                if (bytesRead == 0)
                 {
                     break;
                 }
 
-                if (consumeSectionDelimiters)
+                for (int i = 0; i < bytesRead; i++)
                 {
-                    while (IsSectionDelimiter((byte)b))
-                    {
-                        b = Stream.ReadByte();
+                    // Read to the end of the line in the stream
+                    var b = memoryOwner.Memory.Span[i];
 
-                        if (b == -1)
+                    if (consumeSectionDelimiters)
+                    {
+                        if (IsSectionDelimiter(b))
                         {
-                            break;
+                            continue;
                         }
+
+                        consumeSectionDelimiters = false;
                     }
 
-                    consumeSectionDelimiters = false;
+                    // The relative index into the memory, 0 indexed
+                    var memoryIndex = i - memoryStartIndex;
+
+                    if (IsSectionDelimiter(b))
+                    {
+                        // Current memory represents a row. Write it to be read by the reader.
+                        // Memory should be trimmed to only number of bytes read. It may have 
+                        // been overallocated
+                        QueueSection(memoryStartIndex, memoryIndex+1);
+                        consumeSectionDelimiters = true;
+                        memoryStartIndex = i;
+                    }
+
+                    if (IsSegmentDelimiter(b))
+                    {
+                        segmentHints.Add(memoryIndex);
+                    }
                 }
 
-                var memoryIndex = memoryStartIndex + bytesRead;
-                memoryOwner.Memory.Span[memoryIndex] = (byte)b;
-
-                if (IsSectionDelimiter((byte)b))
+                if (memoryStartIndex == 0)
                 {
-                    // Current memory represents a row. Write it to be read by the reader.
-                    // Memory should be trimmed to only number of bytes read. It may have 
-                    // been overallocated
-                    QueueSection();
-                    consumeSectionDelimiters = true;
+                    // None of the memory was used, check if the whole stream has been read
+                    if (bytesRead == Stream.Length)
+                    {
+                        // The whole memory chunk is a section
+                        QueueSection(0, bytesRead);
+                    }
+                    else
+                    {
+                        throw new OutOfMemoryException($"Unable to allocate enough memory to read section of stream. May be improved in the future");
+                    }
                 }
-                else if (++bytesRead >= memoryOwner.Memory.Length)
+                else if ((memoryStartIndex+1) < bytesRead)
                 {
                     // The current allocated memory has run out of space, need to relocate
                     int rentSize = (int)Stream.Length - (rentedChunks * _memoryPoolRentSize);
                     var tmpMemory = _memoryPool.Rent(Math.Min(rentSize, _memoryPoolRentSize));
 
                     // Only copy memory that's been used
-                    var memToCopy = memoryOwner.Memory.Slice(memoryStartIndex, bytesRead);
+                    var memToCopy = memoryOwner.Memory.Slice(memoryStartIndex, bytesRead-memoryStartIndex);
                     memToCopy.CopyTo(tmpMemory.Memory);
 
                     // Dispose of old memory and save new memory
                     memoryOwner.Dispose();
                     memoryOwner = tmpMemory;
 
-                    // Reset the start index, since old memory is no longer managed here
-                    memoryStartIndex = 0;
+                    // Set the consumed memory index to accomodate the new memory that was copied
+                    consumedMemoryIndex = (bytesRead - memoryStartIndex - 1);
                 }
 
-                if ((bytesRead > 0) && IsSegmentDelimiter((byte)b))
-                {
-                    segmentHints.Add(bytesRead - 1);
-                }
             }
 
-            if (bytesRead != 0)
+            void QueueSection(int start, int size)
             {
-                QueueSection();
-            }
-
-            void QueueSection()
-            {
-                var section = new MemoryManagedSection(memoryOwner, memoryStartIndex, bytesRead, segmentHints, Settings.Encoding);
+                var section = new MemoryManagedSection(memoryOwner, start, size, segmentHints, Settings.Encoding);
                 _queue.Enqueue(section);
                 segmentHints.Clear();
-                memoryStartIndex += bytesRead;
-                bytesRead = 0;
             }
-
-            return Task.CompletedTask;
         }
 
         private class MemoryManagedSection : MemoryOwningSection
